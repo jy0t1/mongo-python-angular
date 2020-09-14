00e388fe06ec802d57ec3a0f128b4f229dd374f2
Jyoti Sarkar <mr_jyoti@yahoo.com>
2020-09-14 18:32:13 -0400

    Add files via upload

--- mongo_api.py
+++ mongo_api.py
@@ -1,4 +1,6 @@
 #this is api that will do data fetch or data isert,update in mongo db
+# intentionally I have made all elemnet names in mongodb collection lowercase because pymongo library allows "insert" with field name, 
+# but mongo-driver for Golang always created elements in lowercase during insert. 
 from pymongo import MongoClient
 from flask import Flask, request, jsonify
 import json
@@ -16,7 +18,7 @@ output = []
 def get_all_books():
   output = []
   for r in records.find():
-      output.append({'id':r['id'],'name':r['name'], 'author':r['author'],'published':r['published'], 'pages':r['pages'],'publisher':r['publisher'], 'isAvailable':r['isAvailable'], 'category':r['category'],'bindType':r['bindType'],'photoPath':r['photoPath']})
+      output.append({'id':r['id'],'name':r['name'], 'author':r['author'],'authorEmail':r['authoremail'], 'published':r['published'], 'pages':r['pages'],'publisher':r['publisher'], 'isAvailable':r['isavailable'], 'category':r['category'],'bindType':r['bindtype'],'photoPath':r['photopath']})
 
   return jsonify(output)
 
@@ -25,10 +27,14 @@ def get_a_book(bookid):
   output = []
   id = int(bookid)
   if id > 0:
-    one_book = records.find_one({'id' : id})
-    output = {'id':one_book['id'],'name':one_book['name'],'author':one_book['author'],'published':one_book['published'], 'pages':one_book['pages'],'publisher':one_book['publisher'], 'isAvailable':one_book['isAvailable'], 'category':one_book['category'],'bindType':one_book['bindType'],'photoPath':one_book['photoPath']}
+    nCount = records.count_documents({'id' : id})
+    if nCount > 0: 
+      one_book = records.find_one({'id' : id})
+      output = {'id':one_book['id'],'name':one_book['name'],'author':one_book['author'],'authorEmail':one_book['authoremail'],'published':one_book['published'], 'pages':one_book['pages'],'publisher':one_book['publisher'], 'isAvailable':one_book['isavailable'], 'category':one_book['category'],'bindType':one_book['bindtype'],'photoPath':one_book['photopath']}
+    else:
+      output = {'count':0,'id':None,'name':None,'author':None,'published':None, 'pages':None,'publisher':None, 'isAvailable':None, 'category':None,'bindType':None,'photoPath':None}
   else:
-    output = {'id':None,'name':None,'author':None,'published':None, 'pages':None,'publisher':None, 'isAvailable':None, 'category':None,'bindType':None,'photoPath':None}
+    output = {'count':0,'id':None,'name':None,'author':None,'published':None, 'pages':None,'publisher':None, 'isAvailable':None, 'category':None,'bindType':None,'photoPath':None}
   return jsonify(output)
 
 @app.route('/books/create', methods=['POST'])
@@ -38,6 +44,7 @@ def add_a_book():
   id = request.json['id']
   name = request.json['name']
   author = request.json['author']
+  authorEmail = request.json['authorEmail']
   published = request.json['published']
   pages = request.json['pages']
   publisher = request.json['publisher']
@@ -46,7 +53,7 @@ def add_a_book():
   bindType = request.json['bindType']
   photoPath = request.json['photoPath']
 
-  book_id = records.insert({'id':bson.int64.Int64(id),'name':name,'author':author,'published':published,'pages':bson.int64.Int64(pages),'publisher':publisher,'isAvailable':isAvailable,'category':category, 'bindType':bindType, 'photoPath':photoPath })
+  book_id = records.insert({'id':bson.int64.Int64(id),'name':name,'author':author,'authoremail':authorEmail,'published':published,'pages':bson.int64.Int64(pages),'publisher':publisher,'isavailable':isAvailable,'category':category, 'bindtype':bindType, 'photopath':photoPath })
   new_book = records.find_one({'_id' : book_id})
   output = {'name':new_book['name'],'page':new_book['pages'],'published':new_book['published']}
   return jsonify({'result' : output})
@@ -59,13 +66,14 @@ def upd_a_book(bookid):
     {'id': id},
     {'$set': {'name': request.json['name'],
           'author': request.json['author'],
+          'authoremail': request.json['authorEmail'],
           'published': request.json['published'],
           'pages': request.json['pages'],
           'publisher': request.json['publisher'],
-          'isAvailable': request.json['isAvailable'],
+          'isavailable': request.json['isAvailable'],
           'category': request.json['category'],
-          'bindType': request.json['bindType'],
-          'photoPath': request.json['photoPath']
+          'bindtype': request.json['bindType'],
+          'photopath': request.json['photoPath']
         }
     }
     )
@@ -79,7 +87,7 @@ def del_a_book(bookid):
   if id > 0:
     myQuery ={'id':id} 
     records.delete_one(myQuery) 
-  output = {'id':None,'name':None,'author':None,'published':None, 'pages':None,'publisher':None, 'isAvailable':None, 'category':None,'bindType':None,'photoPath':None}
+  output = {'id':None,'name':None,'author':None,'authorEmail':None,'published':None, 'pages':None,'publisher':None, 'isAvailable':None, 'category':None,'bindType':None,'photoPath':None}
   return jsonify({'result' : output})
 
 if __name__ == '__main__':
